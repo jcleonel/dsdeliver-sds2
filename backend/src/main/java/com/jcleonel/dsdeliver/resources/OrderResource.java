c8a0a5dba761177486e31f925db60ce32d559a97
jcleonel <jean.leonel.costa@gmail.com>
2021-09-09 18:06:24 -0300

    PUT orders

--- backend/src/main/java/com/jcleonel/dsdeliver/resources/OrderResource.java
+++ backend/src/main/java/com/jcleonel/dsdeliver/resources/OrderResource.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,9 +33,14 @@ public class OrderResource {
 	@PostMapping
 	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO orderDto){
 		orderDto = orderService.insert(orderDto);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("${id}")
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(orderDto.getId()).toUri();
 		return ResponseEntity.created(uri).body(orderDto);
 	}
 
+	@PutMapping("/{id}/delivered")
+	public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id) {
+		OrderDTO orderDto = orderService.setDelivered(id);
+		return ResponseEntity.ok().body(orderDto);
+	}
 }
