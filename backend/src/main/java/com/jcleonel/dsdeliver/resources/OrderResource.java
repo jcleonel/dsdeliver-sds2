d23fd4f95a07f4add5e19796669711a2f11b87db
jcleonel <jean.leonel.costa@gmail.com>
2021-09-09 17:08:37 -0300

    GET orders

---
+++ backend/src/main/java/com/jcleonel/dsdeliver/resources/OrderResource.java
@@ -0,0 +1,27 @@
+package com.jcleonel.dsdeliver.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jcleonel.dsdeliver.dto.OrderDTO;
+import com.jcleonel.dsdeliver.services.OrderService;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderResource {
+
+	@Autowired
+	private OrderService orderService;
+
+	@GetMapping
+	public ResponseEntity<List<OrderDTO>> findAll() {
+		List<OrderDTO> list = orderService.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+}
