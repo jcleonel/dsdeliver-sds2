a6ea89d4f8c69b5aee45c6933eac8d20febfbf81
jcleonel <jean.leonel.costa@gmail.com>
2021-09-12 11:14:33 -0300

    Routes and Add orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/Header';
-import Home from './src/Home';
+import Routes from './src/Routes';
 
 export default function App() {
   return (
     <View style={styles.container}> 
-      <Header />
-      <Home />
+      <Routes />
       <StatusBar style="auto" />
     </View>
   );
--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,9 +1,16 @@
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import AppLoading from 'expo-app-loading';
-import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 function Header() {
+    const navigation = useNavigation();
+    const handleOnPress = () => {
+        navigation.navigate('Home');
+    }
+
     let [fontsLoaded] = useFonts({
         OpenSans_400Regular,
         OpenSans_700Bold
@@ -13,10 +20,12 @@ function Header() {
         return <AppLoading />;
     }
     return (
-        <View style={styles.container}>
-            <Image source={require('../assets/logo.png')} />
-            <Text style={styles.text}>DS Delivery</Text>     
-        </View>
+        <TouchableWithoutFeedback onPress={handleOnPress}>
+            <View style={styles.container}>
+                <Image source={require('../assets/logo.png')} />
+                <Text style={styles.text}>DS Delivery</Text>     
+            </View>
+        </TouchableWithoutFeedback>
     );
 }
 
